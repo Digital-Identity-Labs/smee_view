357bae32b8487c7ee7ea18dd7b406a9d0a889604
Pete Birkinshaw <pete@binary-ape.org>
2023-04-25 17:35:13 +0100

    Full mix config, with docs groups because there are so many modules

--- mix.exs
+++ mix.exs
@@ -6,9 +6,34 @@ defmodule SmeeView.MixProject do
       app: :smee_view,
       version: "0.1.0",
       elixir: "~> 1.14",
+      description: "Metadata parsing extension for Smee",
+      package: package(),
+      name: "SmeeView",
+      source_url: "https://github.com/Digital-Identity-Labs/smee_view",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      test_coverage: [
+        tool: ExCoveralls
+      ],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      docs: [
+        main: "readme",
+        # logo: "path/to/logo.png",
+        extras: ["README.md", "LICENSE"],
+        groups_for_modules: [
+          "Views / Collections": view_modules(),
+          "Aspects / Items": aspect_modules(),
+          "Protocols & Behaviours": protocol_modules()
+        ]
+      ],
       deps: deps(),
-      compilers: Mix.compilers() ++ [:rambo], # Needed until issue fixed in Rambo
+      compilers: Mix.compilers(),
+      elixirc_paths: elixirc_paths(Mix.env)
     ]
   end
 
@@ -18,6 +43,8 @@ defmodule SmeeView.MixProject do
       extra_applications: [:logger]
     ]
   end
+
+
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
@@ -40,4 +67,115 @@ defmodule SmeeView.MixProject do
       {:doctor, "~> 0.17.0", only: :dev, runtime: false}
     ]
   end
+
+  defp package() do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => "https://github.com/Digital-Identity-Labs/smee"
+      }
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support", "priv"]
+  defp elixirc_paths(_), do: ["lib", "priv"]
+
+  defp protocol_modules do
+    [SmeeView.Protocols.Aspect, SmeeView.Behaviours.Aspect, SmeeView.Behaviours.View]
+  end
+
+  defp view_modules do
+    [
+      SmeeView.ArtifactResolutionServices,
+      SmeeView.AspectFilter,
+      SmeeView.AssertionConsumerServices,
+      SmeeView.AssertionIDRequestServices,
+      SmeeView.AttributeConsumingServices,
+      SmeeView.AttributeServices,
+      SmeeView.Contacts,
+      SmeeView.Descriptions,
+      SmeeView.DiscoHints,
+      SmeeView.DiscoveryResponses,
+      SmeeView.Displaynames,
+      SmeeView.Domains,
+      SmeeView.Entity,
+      SmeeView.EntityFilter,
+      SmeeView.Geolocations,
+      SmeeView.IdP,
+      SmeeView.Keys,
+      SmeeView.Keywords,
+      SmeeView.Logos,
+      SmeeView.ManageNameidServices,
+      SmeeView.NameIDFormats,
+      SmeeView.NameidMappingServices,
+      SmeeView.Networks,
+      SmeeView.Organization,
+      SmeeView.OrganizationDisplaynames,
+      SmeeView.OrganizationNames,
+      SmeeView.OrganizationURLs,
+      SmeeView.PublicationPath,
+      SmeeView.Registration,
+      SmeeView.RegistrationPolicies,
+      SmeeView.RequestInitiators,
+      SmeeView.RequestedAttributes,
+      SmeeView.Roles,
+      SmeeView.SP,
+      SmeeView.Scopes,
+      SmeeView.ServiceDescriptions,
+      SmeeView.ServiceNames,
+      SmeeView.Services,
+      SmeeView.SingleLogoutServices,
+      SmeeView.SingleSignonServices,
+      SmeeView.UI
+    ]
+  end
+
+  defp aspect_modules do
+    [
+      SmeeView.Aspects.ArtifactResolutionService,
+      SmeeView.Aspects.AspectCommon,
+      SmeeView.Aspects.AspectTools.Text,
+      SmeeView.Aspects.AssertionConsumerService,
+      SmeeView.Aspects.AssertionIDRequestService,
+      SmeeView.Aspects.AttributeConsumingService,
+      SmeeView.Aspects.AttributeService,
+      SmeeView.Aspects.Contact,
+      SmeeView.Aspects.Description,
+      SmeeView.Aspects.DigestMethod,
+      SmeeView.Aspects.DiscoveryResponse,
+      SmeeView.Aspects.Displayname,
+      SmeeView.Aspects.Domain,
+      SmeeView.Aspects.EncryptionMethod,
+      SmeeView.Aspects.Entity,
+      SmeeView.Aspects.Geolocation,
+      SmeeView.Aspects.IdP,
+      SmeeView.Aspects.InformationURL,
+      SmeeView.Aspects.Key,
+      SmeeView.Aspects.Keywords,
+      SmeeView.Aspects.Logo,
+      SmeeView.Aspects.ManageNameidService,
+      SmeeView.Aspects.NameIDFormat,
+      SmeeView.Aspects.NameidMappingService,
+      SmeeView.Aspects.Network,
+      SmeeView.Aspects.Organization,
+      SmeeView.Aspects.OrganizationDisplayname,
+      SmeeView.Aspects.OrganizationName,
+      SmeeView.Aspects.OrganizationURL,
+      SmeeView.Aspects.PrivacyURL,
+      SmeeView.Aspects.Publication,
+      SmeeView.Aspects.Registration,
+      SmeeView.Aspects.RegistrationPolicy,
+      SmeeView.Aspects.RequestInitiator,
+      SmeeView.Aspects.RequestedAttribute,
+      SmeeView.Aspects.SP,
+      SmeeView.Aspects.Scope,
+      SmeeView.Aspects.ServiceDescription,
+      SmeeView.Aspects.ServiceName,
+      SmeeView.Aspects.SigningMethod,
+      SmeeView.Aspects.SingleLogoutService,
+      SmeeView.Aspects.SingleSignonService
+    ]
+  end
+
 end
