f8cbf141695e8fa4523acb2bc4c6cd8f48e0a609
Pete Birkinshaw <pete@binary-ape.org>
2023-05-28 20:47:53 +0100

    Boilerplate template moduledoc for all View modules (element names are wrong except for one)

--- lib/smee_view/publication_path.ex
+++ lib/smee_view/publication_path.ex
@@ -1,5 +1,22 @@
 defmodule SmeeView.PublicationPath do
 
+  @moduledoc """
+  This module can extracts the `<ArtifactResolutionService>` elements in entity metadata as "aspect" structs, and then
+  process and filter the results.
+
+  Aspects can be extracted with three functions:
+
+  * Views, via `view/2` are lists of aspects
+  * Prisms, via `prism/2` are maps of entity IDs to views
+  * There is also `view_one/2` which returns the first aspect of one entity
+
+  Aspects can be extracted from `Smee.Metadata` structs, `Smee.Entity` structs, or lists of aspects, Metadata and Entity
+    structs.
+
+  The other functions in this module are intended to be applied to collections of aspect structs - for working with
+    individual aspects, please look at the appropriate Aspect module.
+  """
+
   use SmeeView.ViewCommon, aspect: SmeeView.Aspects.Publication, roles: false
 
   @entity_xmap [
