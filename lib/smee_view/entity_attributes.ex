c9eb1b17694e20dc3673da8cd0a4436ce100f479
Pete Birkinshaw <pete@binary-ape.org>
2023-05-30 10:30:10 +0100

    Specs for entity attributes

--- lib/smee_view/entity_attributes.ex
+++ lib/smee_view/entity_attributes.ex
@@ -25,18 +25,21 @@ defmodule SmeeView.EntityAttributes do
     values: ~x"string(saml:AttributeValue)"ls
   ]
 
+  @spec contain?(aspects :: list() | map(), name:: binary()) :: list() | map()
   def contain?(aspects, name) when is_map(aspects), do: prismify(aspects, name, &contain?/2)
   def contain?(aspects, name) do
     aspects
     |> Enum.any?(fn a -> a.name == name end)
   end
 
+  @spec contain?(aspects :: list() | map(), name:: binary(), value :: binary()) :: list() | map()
   def contain?(aspects, name, value) when is_map(aspects), do: prismify(aspects, name, value, &contain?/3)
   def contain?(aspects, name, value) do
     aspects
     |> Enum.any?(fn a -> a.name == name && Enum.any?(a.values, fn v -> v == value end) end)
   end
 
+  @spec values_for(aspects :: list() | map(), name:: binary()) :: list() | map()
   def values_for(aspects, name) when is_map(aspects), do: prismify(aspects, name, &values_for/2)
   def values_for(aspects, name) do
     aspects
@@ -51,6 +54,7 @@ defmodule SmeeView.EntityAttributes do
     |> Enum.sort()
   end
 
+  @spec names(aspects :: list() | map()) :: list() | map()
   def names(aspects) when is_map(aspects), do: prismify(aspects, &names/1)
   def names(aspects) do
     aspects
@@ -64,6 +68,7 @@ defmodule SmeeView.EntityAttributes do
     |> Enum.sort()
   end
 
+  @spec values(aspects :: list() | map()) :: list() | map()
   def values(aspects) when is_map(aspects), do: prismify(aspects, &values/1)
   def values(aspects) do
     aspects
@@ -77,24 +82,28 @@ defmodule SmeeView.EntityAttributes do
     |> Enum.sort()
   end
 
+  @spec assurance_certifications(aspects :: list() | map()) :: list() | map()
   def assurance_certifications(aspects) when is_map(aspects), do: prismify(aspects, &assurance_certifications/1)
   def assurance_certifications(aspects) do
     aspects
     |> values_for("urn:oasis:names:tc:SAML:attribute:assurance-certification")
   end
 
+  @spec entity_categories(aspects :: list() | map()) :: list() | map()
   def entity_categories(aspects) when is_map(aspects), do: prismify(aspects, &entity_categories/1)
   def entity_categories(aspects) do
     aspects
     |> values_for("http://macedir.org/entity-category")
   end
 
+  @spec supports_entity_categories(aspects :: list() | map()) :: list() | map()
   def supports_entity_categories(aspects) when is_map(aspects), do: prismify(aspects, &supports_entity_categories/1)
   def supports_entity_categories(aspects) do
     aspects
     |> values_for("http://macedir.org/entity-category-support")
   end
 
+  @spec category?(aspects :: list() | map(), category :: binary()) :: list() | map()
   def category?(aspects, category) when is_map(aspects), do: prismify(aspects, category, &category?/2)
   def category?(aspects, category) do
     aspects
@@ -102,6 +111,7 @@ defmodule SmeeView.EntityAttributes do
     |> Enum.member?(category)
   end
 
+  @spec supports?(aspects :: list() | map(), category :: binary()) :: list() | map()
   def supports?(aspects, category) when is_map(aspects), do: prismify(aspects, category, &supports?/2)
   def supports?(aspects, category) do
     aspects
@@ -109,30 +119,35 @@ defmodule SmeeView.EntityAttributes do
     |> Enum.member?(category)
   end
 
+  @spec ras?(aspects :: list() | map()) :: list() | map()
   def ras?(aspects) when is_map(aspects), do: prismify(aspects, &ras?/1)
   def ras?(aspects) do
     aspects
     |> contain?("http://macedir.org/entity-category", "http://refeds.org/category/research-and-scholarship")
   end
 
+  @spec sirtfi?(aspects :: list() | map()) :: list() | map()
   def sirtfi?(aspects) when is_map(aspects), do: prismify(aspects, &sirtfi?/1)
   def sirtfi?(aspects) do
     aspects
     |> contain?("urn:oasis:names:tc:SAML:attribute:assurance-certification", "https://refeds.org/sirtfi")
   end
 
+  @spec coco?(aspects :: list() | map()) :: list() | map()
   def coco?(aspects) when is_map(aspects), do: prismify(aspects, &coco?/1)
   def coco?(aspects) do
     aspects
     |> contain?("http://macedir.org/entity-category", "https://refeds.org/category/code-of-conduct/v2")
   end
 
+  @spec hide?(aspects :: list() | map()) :: list() | map()
   def hide?(aspects) when is_map(aspects), do: prismify(aspects, &hide?/1)
   def hide?(aspects) do
     aspects
     |> contain?("http://macedir.org/entity-category", "http://refeds.org/category/hide-from-discovery")
   end
 
+  @spec required_subject_ids(aspects :: list() | map()) :: list() | map()
   def required_subject_ids(aspects) when is_map(aspects), do: prismify(aspects, &required_subject_ids/1)
   def required_subject_ids(aspects) do
     aspects
@@ -144,6 +159,5 @@ defmodule SmeeView.EntityAttributes do
   defp entity_xmap do
     @entity_xmap
   end
-
 
 end
