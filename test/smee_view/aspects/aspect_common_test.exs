911dce16b12cf22c41f792cbba7d42f20ba17328
Pete Birkinshaw <pete@binary-ape.org>
2023-06-01 08:18:26 +0100

    Can't test this directly really

--- test/smee_view/aspects/aspect_common_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule SmeeViewAspectsDisplaynameTest do
-  use ExUnit.Case
-
- # use AspectTextSharedTests, aspect: true
-
- #AspectFixtures.fixtures(SmeeView.Aspects.Displayname, [%{lang: "en", text: "aha"}])
-
-
-end
